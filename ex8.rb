4ca144a75aadfe7bb947e6bb3f99fdd3aa0043f3
Filip Stankovic <filips@gmail.com>
2016-01-13 22:00:49 -0500

    Exercise 8

---
+++ ex8.rb
@@ -0,0 +1,13 @@
+# Problem Statement
+# Compute the sum of cubes for a given range a through b.
+#
+# Write a method called sum_of_cubes to accomplish this task
+#
+# Example Given range 1 to 3 the method should return 36
+
+def sum_of_cubes(a, b)
+  puts (a..b).inject(0) {|sum, cube| sum += cube**3}
+end
+#
+sum_of_cubes(3,5) #should be 216
+sum_of_cubes(1,3) #should be 36
